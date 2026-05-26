7563cb5a0d0066e741b1adb0ca381a168422016d
arlosmasher-coder <arlosmasher@gmail.com>
2026-05-26 16:53:28 +0530

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * github-profile-snapshot
+ * Generate reproducible JSON snapshots of GitHub profiles for audits and diffs.
+ */
+
+export { GithubProfileSnapshot } from "./github-restgraphql-fetch-with-";
+export type { GithubProfileSnapshotOptions, GithubProfileSnapshotResult } from "./types";
+export { GithubProfileSnapshotError, ConfigurationError, ValidationError } from "./errors";
